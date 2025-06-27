ab63ac2635573e8b2763cd1ecea4bbaf8395c30f
martindipeco <martindipeco@gmail.com>
2025-06-26 23:23:41 -0300

    agrego linea para defaultear a USER

--- src/main/java/com/inquisitorius/skillslink/controller/RegistroController.java
+++ src/main/java/com/inquisitorius/skillslink/controller/RegistroController.java
@@ -1,6 +1,7 @@
 package com.inquisitorius.skillslink.controller;
 
 import com.inquisitorius.skillslink.domain.usuario.DatosRegistrarUsuario;
+import com.inquisitorius.skillslink.domain.usuario.Rol;
 import com.inquisitorius.skillslink.domain.usuario.Usuario;
 import com.inquisitorius.skillslink.repository.UsuarioRepository;
 import jakarta.validation.Valid;
@@ -27,6 +28,13 @@ public class RegistroController {
         if (usuarioRepository.findByLogin(datosRegistrarUsuario.login()) != null) {
             return ResponseEntity.badRequest().build();
         }
+
+    Rol rol;
+    if (datosRegistrarUsuario.rol() != null) {
+        rol = datosRegistrarUsuario.rol();
+    } else {
+        rol = Rol.USER;
+    }
 
         String encryptedPassword = passwordEncoder.encode(datosRegistrarUsuario.clave());
         Usuario nuevoUsuario = new Usuario(datosRegistrarUsuario.login(), encryptedPassword, datosRegistrarUsuario.rol());
