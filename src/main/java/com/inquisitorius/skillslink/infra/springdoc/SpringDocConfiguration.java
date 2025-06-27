368c685ab51b4ce6b679cfa17ed6377fbc15a660
martindipeco <martindipeco@gmail.com>
2025-06-27 09:00:44 -0300

    fix: modificaciones en documentacion para https

--- src/main/java/com/inquisitorius/skillslink/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/com/inquisitorius/skillslink/infra/springdoc/SpringDocConfiguration.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,10 @@ public class SpringDocConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://inquisitorius-production.up.railway.app"))
+                .addServersItem(new Server().url("https://inquisitorius-production-7e73.up.railway.app"))
+                .addServersItem(new Server().url("https://https://inquisitorius.onrender.com"))
+                .addServersItem(new Server().url("http://localhost:8080"))
                 .components(new Components().addSecuritySchemes("bearer-key",
                         new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
