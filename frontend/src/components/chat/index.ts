d47978bc78ba1060f0ca5c9aa233bd68042b223d
Brayan Galindez <80848445+BrayanGalindez@users.noreply.github.com>
2025-06-28 12:33:56 -0500

    Delete frontend directory

--- frontend/src/components/chat/index.ts
+++
@@ -1,5 +0,0 @@
-export { Chat } from './Chat';
-export { ChatWindow } from './ChatWindow';
-export { ConversationList } from './ConversationList';
-export { MessageBubble } from './MessageBubble';
-export { MessageInput } from './MessageInput'; 
