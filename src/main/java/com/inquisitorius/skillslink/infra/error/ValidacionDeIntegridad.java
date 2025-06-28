561b4447479c0208f6cd01ede2ad0b3e037153b6
Martin Di Peco <martindipeco@gmail.com>
2025-06-28 14:05:10 -0300

    Delete src/main/java/com/inquisitorius/skillslink/infra directory

--- src/main/java/com/inquisitorius/skillslink/infra/error/ValidacionDeIntegridad.java
+++
@@ -1,7 +0,0 @@
-package com.inquisitorius.skillslink.infra.error;
-
-public class ValidacionDeIntegridad extends RuntimeException {
-    public ValidacionDeIntegridad(String s) {
-        super(s);
-    }
-}
--- src/main/java/com/inquisitorius/skillslink/infra/security/DatosJwToken.java
+++
@@ -1,4 +0,0 @@
-package com.inquisitorius.skillslink.infra.security;
-
-public record DatosJwToken(String jwToken) {
-}
